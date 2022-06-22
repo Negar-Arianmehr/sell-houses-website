c35b5f0a59c4ff39d8342e95a315da486cb2adec
Negar-Arianmehr <negar.arianmehr89@gmail.com>
2022-06-22 17:06:16 +0200

    calc

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const calc = (a, b) => {
+  console.log(
+    `Addition: ${a + b}, Subtraction: ${a - b}, Multiplication: ${
+      a * b
+    }, Division: ${a / b}`
+  );
+};
+
+calc(9, 3);
+calc(15, 85);
+calc(68, 23);
